ba3e3ab3abe7d794af914c1b66811aeaa39cad76
tefiavalos <emlavalos92@gmail.com>
2020-03-25 21:41:39 -0300

    tarea

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from 'styled-components';
+
+const LaLoz = styles.div`
+  height: 500px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .luzApagada {
+    background: #000;
+    button{
+      background-color: lightyellow;
+      color: black;
+    }
+  }
+  .luzPrendida {
+    background: lightyellow;
+     button{
+      background-color: black;
+      color: lightyellow;
+     }
+  }
+  .nombreDelDiv {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+      button{
+        border: none;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+      }
+  }
+
+`;
 
 function App() {
+  let [color, setColor] = useState("luzPrendida");
+  let [mensaje, setMensaje] = useState("Apagá la luz")
+
+  const handleClick = () => {
+    console.log("prendo o hago algo con la loooozz MAMA");
+    setColor(color === "luzPrendida" ? "luzApagada" : "luzPrendida")
+    setMensaje(mensaje === "Apagá la luz" ? "Prendé la luz" : "Apagá la luz")
+    // "CORTASTE TODA LA LOOOZ"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LaLoz>
+      <div className={`nombreDelDiv ${color}`}>
+      <button onClick={handleClick}>{mensaje}</button>
+      </div>
+    </LaLoz>
   );
 }
 
